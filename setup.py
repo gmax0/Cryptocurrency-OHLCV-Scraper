f687e9f8e566ebc93cab84f6e34c7a9bf9713d34
gaamox@tutanota.com <gaamox@tutanota.com>
2022-04-06 20:18:05 -0700

    Add PyPi files

--- setup.py
+++ setup.py
@@ -2,6 +2,15 @@ from setuptools import setup, find_packages
 
 setup(
     name='py_cc_ohlcv',
-    version='0.1.0',
-    packages=find_packages(exclude=["examples"])
+    version='0.0.1',
+    license='MIT',
+    author="Max Gao",
+    author_email='gaamox@tutanota.com',
+    packages=find_packages(exclude=["examples"]),
+    url='https://github.com/gmax0/Cryptocurrency-OHLCV-Scraper',
+    keywords='ohlcv, cryptocurrency, historical, market, data',
+    install_requires=[
+        'pandas',
+        'requests'
+      ],
 )
