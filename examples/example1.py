2de0fc4355e1d792b85f598343937588f3f9ce91
gaamox@tutanota.com <gaamox@tutanota.com>
2022-04-06 20:00:14 -0700

    CoinbasePro, Binance, Ftx, Kucoin 1m OHLCV functional

--- examples/example1.py
+++ examples/example1.py
@@ -1,11 +1,26 @@
-from py_cc_ohlcv import scraper, exchanges, instruments
+from py_cc_ohlcv import scraper, exchanges
 from datetime import datetime, timezone, timedelta
+import logging
 
-# Set a window from [present - 24 hours, present]
-end_date = datetime.now(timezone.utc)
-start_date = end_date - timedelta(hours=24) 
+logging.basicConfig(level = logging.INFO)
+
+# Set a start and end date
+start_date = datetime(2022, 1, 1)
+start_date = start_date.replace(tzinfo=timezone.utc)
+end_date = start_date + timedelta(hours=24)
 
 # Initialize Scraper
-cb_scraper = scraper.Scraper(exchanges.COINBASE_PRO, instruments.BTC_USD, start_date, end_date)
-cb_scraper.set_http_proxy('http://0.0.0.0:8000') 
+#cb_scraper = scraper.Scraper(exchanges.COINBASE_PRO, "BTC_USD", "1m", start_date, end_date)
+#cb_scraper = scraper.Scraper(exchanges.BINANCE, "BTC_USDT", "1m", start_date, end_date)
+#cb_scraper = scraper.Scraper(exchanges.FTX, "BTC_USD", "1m", start_date, end_date)
+cb_scraper = scraper.Scraper(exchanges.KUCOIN, "BTC_USDT", "1m", start_date, end_date)
 
+proxies = {
+    "http": "http://0.0.0.0:8000",
+    "https": "https://0.0.0.0:8000",
+}
+#cb_scraper.set_proxies(proxies)
+
+# Begin scrapping
+candles_df = cb_scraper.run()
+print(candles_df)
